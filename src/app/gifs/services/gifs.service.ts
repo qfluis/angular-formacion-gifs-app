5529040125add59c0c6baf941fc13c628c9f24ac
Luis Quevedo <qfluis@gmail.com>
2022-02-19 15:02:37 +0100

    Video 91

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Data } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,13 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private apiKey:string = "yN8WtDyAPJq8jZY0jeoN1SrgzT0Uzc6m";
+  private servicioUrl:string = "https://api.giphy.com/v1/gifs";
+
+
   private _historial: string[] = []
 
   // TODO: cambiar tipado
-  public resultados:any[]=[];
+  public resultados:Data[]=[];
 
   
   get historial() {
@@ -24,9 +28,11 @@ export class GifsService {
     if(!this._historial.includes(query)) {
       this._historial.unshift(query);
       if (this._historial.length > 10) this._historial.pop();
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
     
-    console.log(this._historial);
+    //console.log(this._historial);
     
     /* FETCH
     fetch('https://api.giphy.com/v1/gifs/search?api_key=yN8WtDyAPJq8jZY0jeoN1SrgzT0Uzc6m&q=dragon ball z&limit=10&offset=0&rating=g&lang=es')
@@ -41,19 +47,30 @@ export class GifsService {
     const data = await resp.json();
     console.log(data.data[0].images.downsized_large.url);
     */
-
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=10&offset=0&rating=g&lang=es`)
-      .subscribe( (resp:any) => {
-        console.log(resp.data);
-        this.resultados = resp.data
-      });
-
     
-  
-  
+    const params = new HttpParams()
+        .set('api_key', this.apiKey)
+        .set('limit', '10')
+        .set('q', query)
+        .set('offset', '0')
+        .set('rating','g')
+        .set('lang','es');
+
+
+    this.http.get<SearchGifsResponse>(`${ this.servicioUrl }/search`, { params}) // {params} === {params:params}
+      .subscribe( (resp:SearchGifsResponse) => {
+        //console.log(resp);
+        this.resultados = resp.data;
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      });  
   }
 
-  constructor( private http:HttpClient){}
+  constructor( private http:HttpClient){
+
+    this._historial = JSON.parse(localStorage.getItem("historial") || "[]");
+    this.resultados = JSON.parse(localStorage.getItem("resultados") || "[]");
+
+  }
 
 
 
